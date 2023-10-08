e84c1e68203aa6abd5c8cfa36a01b1fdefaf8d25
ljeagle <vincent.awadb@gmail.com>
2023-10-08 22:40:03 +0800

    docs test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'nature'
 html_static_path = ['_static']
