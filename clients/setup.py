2d58b2bd4b31ea1e721ec6a935d92dcd0d1ac541
ljeagle <vincent.awadb@gmail.com>
2023-12-16 17:13:49 +0800

    update setup.py

--- clients/setup.py
+++ clients/setup.py
@@ -8,6 +8,8 @@ from setuptools import setup
 from setuptools.command.build_ext import build_ext
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
@@ -17,9 +19,10 @@ setup(
     author = "Vincent",
     author_email = "awadb.vincent@gmail.com",
     description = "Python client for AI Native database AwaDB",
-    long_description = "Python client for AI Native database AwaDB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license = "Apache 2.0",
-    keywords = "awadb-client python sdk vectordb",
+    keywords = ["awadb-client", "python sdk", "vectordb", "AI Native SearchEngine", "RAG"],
     url = "https://github.com/awa-ai/awadb",
     packages = ["awadb_client", "awadb_client.py_idl"],
 
